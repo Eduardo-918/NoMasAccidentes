d41e1c393a3a868b69f086e82bce2fd3a96ca7ec
Kevin <ke.vergara@duocuc.cl>
2026-04-28 16:21:13 -0400

    Commit inicial: Proyecto base Spring Boot

---
+++ src/test/java/com/example/NoMasAccidentes/NoMasAccidentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NoMasAccidentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoMasAccidentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
