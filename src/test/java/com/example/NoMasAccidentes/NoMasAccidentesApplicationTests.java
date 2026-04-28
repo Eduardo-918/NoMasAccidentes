7d9543c9a150bdd77695f2f43b48a5ed86c55b94
NicolasLavinLoyola <ni.lavinl@duocuc.cl>
2026-04-28 18:48:59 -0400

    Configuracion de proyecto y perfiles dev/prod, mas manejo global de errores y entidad base auditoria y configuracion flyway con migration V1 y perfil de test con H2

--- src/test/java/com/example/NoMasAccidentes/NoMasAccidentesApplicationTests.java
+++ src/test/java/com/example/NoMasAccidentes/NoMasAccidentesApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.NoMasAccidentes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles
 class NoMasAccidentesApplicationTests {
 
 	@Test
